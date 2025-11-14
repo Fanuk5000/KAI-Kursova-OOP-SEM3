a4ef221f6e66f3f50810a3f34241ca18c0faba30
Fanuk <illyamak24@gmail.com>
2025-11-14 19:06:24 +0200

    minor abs class fix

--- DAL/Entities/abs_layers.py
+++ DAL/Entities/abs_layers.py
@@ -1,43 +1,54 @@
 from abc import ABC, abstractmethod
+from multiprocessing.util import abstract_sockets_supported
 
-class ABCFootballPlayer(ABC):    
+class ABCFootballPlayer(ABC):  
+    @abstractmethod  
     def __str__(self) -> str: # type: ignore
         pass
 
+    @abstractmethod
     def to_dict(self) -> dict: # type: ignore
         pass
     
     @classmethod
+    @abstractmethod
     def from_dict(cls, data: dict) -> 'FootballPlayer': # type: ignore
         pass
 
 class ABCFootballMatch(ABC):
 
+    @abstractmethod
     def add_player(self, player: 'FootballPlayer') -> None: # type: ignore
         pass
     
+    @abstractmethod
     def remove_player(self, player_id: str) -> None:
         pass
-
+    
+    @abstractmethod
     def to_dict(self) -> dict: # type: ignore
         pass
 
     @classmethod
+    @abstractmethod
     def from_dict(cls, data: dict) -> 'FootballMatch': # type: ignore
         pass
 
+    @abstractmethod
     def __str__(self) -> str: # type: ignore
         pass
 
 
 class ABCFootballStadium(ABC):
-
+    @abstractmethod
     def to_dict(self) -> dict: # type: ignore
         pass
-
+    
+    @abstractmethod
     @classmethod
     def from_dict(cls, data: dict) -> 'FootballStadium': # type: ignore
         pass
 
+    @abstractmethod
     def __str__(self) -> str: # type: ignore
         pass
